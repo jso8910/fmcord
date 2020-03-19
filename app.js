a01ae101e03aea498cd96a78d3f4faaf440a5b8b
‮ymmaj <69@users.noreply.github.com>
2020-03-19 20:14:45 +0100

    Update to v1.0.0 (#5)
    * exit with code 1 on failure, change var to const on trackStream variable
    * bump version to v1.0.0
    * rename keys.json to config.json
    * bump discord-rpc version
    * rename config.json to config.json.example
    * add start option to package.json scripts
    * remove unnecessary line spacing

--- app.js
+++ app.js
@@ -1,37 +1,55 @@
 const DiscordRPC = require('discord-rpc'),
-      {LastFmNode} = require('lastfm'),
+      { LastFmNode } = require('lastfm'),
+      fs = require('fs'),
       log = require("fancy-log");
 
-const keys = require('./keys.json');
+if(fs.existsSync('keys.json')) {
+  fs.renameSync('keys.json', 'config.json');
+  log.info('Renamed keys.json to config.json');
+}
 
-const rpc = new DiscordRPC.Client({ transport: keys.rpcTransportType }),
-      clientId = keys.appClientID,
-      lastFm = new LastFmNode({ api_key: keys.lastFmKey, useragent: 'fmcord v0.0.2' });
+if(!fs.existsSync('config.json')) {
+  fs.copyFileSync('config.example.json', 'config.json')
+  log.info('Created config.json. Please enter your username in the value of `lastFmUsername`.');
+  return;
+}
 
-if(!keys.lastFmUsername) { log.error("Your last.fm username isn't set! Please set it in your keys.json file."); process.exit(0); }
-var trackStream = lastFm.stream(keys.lastFmUsername);
+const {
+  appClientID,
+  imageKeys,
+  rpcTransportType: transport,
+  lastFmKey,
+  lastFmUsername,
+} = require('./config');
 
+const rpc = new DiscordRPC.Client({ transport }),
+      clientId = appClientID,
+      lastFm = new LastFmNode({ api_key: lastFmKey, useragent: 'fmcord v1.0.0' });
+
+if(!lastFmUsername) {
+  log.error("Your last.fm username isn't set! Please set it in your config.json file.");
+  process.exit(1);
+}
+
+const trackStream = lastFm.stream(lastFmUsername);
 
 trackStream.on('nowPlaying', song => {
   if(!song) return;
   rpc.setActivity({
     details: `🎵  ${song.name}`,
     state: `👤  ${song.artist["#text"]}`,
-    largeImageKey: keys.imageKeys.large,
-    smallImageKey: keys.imageKeys.small,
-    largeImageText: `⛓  ${song.url}`,
-    smallImageText: `💿  ${song.album["#text"]}`,
+    largeImageKey: imageKeys.large,
+    smallImageKey: imageKeys.small,
+    smallImageText: `💿 ${song.album["#text"]}`,
     instance: false,
   });
 
   log.info(`Updated song to: ${song.artist["#text"]} - ${song.name}`);
 });
 
-
-
 rpc.on('ready', () => {
   log(`Connected to Discord! (${clientId})`);
   trackStream.start();
 });
 
-rpc.login({clientId}).catch(log.error);
+rpc.login({ clientId }).catch(log.error);
